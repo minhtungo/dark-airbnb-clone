631e8ee31a68022b6ebce87df3901d0455e48ff2
Minh Tu Ngo <loducfc@gmail.com>
2023-04-01 22:49:35 -0400

    feat: add button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { FC } from 'react';
 import { IconType } from 'react-icons';
 
@@ -10,8 +12,48 @@ interface ButtonProps {
   icon?: IconType;
 }
 
-const Button: FC<ButtonProps> = ({ label }) => {
-  return <button>{label}</button>;
+const Button: FC<ButtonProps> = ({
+  label,
+  onClick,
+  disabled,
+  outline,
+  small,
+  icon: Icon,
+}) => {
+  return (
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      className={`
+        relative
+        w-full
+        rounded-lg
+        transition
+        hover:opacity-80
+        disabled:cursor-not-allowed
+        disabled:opacity-70
+        ${outline ? 'bg-neutral-800' : 'bg-rose-500'}
+        ${outline ? 'border-black' : 'border-rose-500'}
+        ${outline ? 'text-white' : 'text-white'}
+        ${small ? 'text-sm' : 'text-md'}
+        ${small ? 'py-1' : 'py-3'}
+        ${small ? 'font-light' : 'font-semibold'}
+        ${small ? 'border' : 'border-2'}
+      `}
+    >
+      {Icon && (
+        <Icon
+          size={24}
+          className='
+            absolute
+            left-4
+            top-3
+          '
+        />
+      )}
+      {label}
+    </button>
+  );
 };
 
 export default Button;
