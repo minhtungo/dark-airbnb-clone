0b349dba04b8757036a4e66a377031dca8fb5e5c
Minh Tu Ngo <loducfc@gmail.com>
2023-04-04 00:53:15 -0400

    feat: show empty state or listing on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <div className='text-4xl'></div>;
+import getCurrentUser from '@/actions/getCurrentUser';
+import getListings from '@/actions/getListings';
+import { ListingCard } from '@/components/listing';
+import { Container } from '@/components/ui';
+import EmptyState from '@/components/ui/EmptyState';
+
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return <EmptyState showReset />;
+  }
+
+  return (
+    <Container>
+      <div className='grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
+        {listings.map((listing) => (
+          <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />
+        ))}
+      </div>
+    </Container>
+  );
 }
