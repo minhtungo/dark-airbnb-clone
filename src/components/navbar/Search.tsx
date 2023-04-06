4a74991a7f911c2160e7975fd65fbb498a081fc9
Minh Tu Ngo <loducfc@gmail.com>
2023-04-06 15:42:12 -0400

    feat: search bar will show current query

--- src/components/navbar/Search.tsx
+++ src/components/navbar/Search.tsx
@@ -2,6 +2,7 @@
 
 import useCountries from '@/hooks/useCountries';
 import useSearchModal from '@/hooks/useSearch';
+import { differenceInDays } from 'date-fns';
 import { useSearchParams } from 'next/navigation';
 import { FC } from 'react';
 import { BiSearch } from 'react-icons/bi';
@@ -13,10 +14,32 @@ const Search: FC<SearchProps> = ({}) => {
   const params = useSearchParams();
   const { getByValue } = useCountries();
 
-  const locationValue = params?.get('locationValue');
+  const location = params?.get('location');
   const startDate = params?.get('startDate');
   const endDate = params?.get('endDate');
   const guestCount = params?.get('guestCount');
+
+  const locationLabel = location
+    ? getByValue(location as string)?.label
+    : 'Anywhere';
+
+  const durationLabel = () => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+      let diff = differenceInDays(end, start);
+
+      if (diff === 0) {
+        diff = 1;
+      }
+
+      return `${diff} Days`;
+    }
+
+    return 'Any Week';
+  };
+
+  const guestLabel = guestCount ? `${guestCount} Guests` : 'Add Guest';
 
   return (
     <div
@@ -24,12 +47,12 @@ const Search: FC<SearchProps> = ({}) => {
       className='w-full cursor-pointer rounded-full border border-gray-800/90 py-2 shadow-sm transition hover:shadow-md md:w-auto'
     >
       <div className='flex flex-row items-center justify-between'>
-        <div className='px-6 text-sm font-medium'>Anywhere</div>
+        <div className='px-6 text-sm font-medium'>{locationLabel}</div>
         <div className='hidden flex-1 border-x px-6 text-center text-sm font-medium sm:block'>
-          Any Week
+          {durationLabel()}
         </div>
         <div className='flex flex-row items-center gap-3 pl-6 pr-2 text-sm text-gray-300'>
-          <div className='hidden sm:block'>Add Guest</div>
+          <div className='hidden sm:block'>{guestLabel}s</div>
           <div className='rounded-full bg-rose-500 p-2 text-white'>
             <BiSearch size={18} />
           </div>
